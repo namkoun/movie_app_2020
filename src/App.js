acc1dd7ce2fe97204f2fa624d2a74d3b93d9742c
namkoun <skarud0328@gmail.com>
2020-10-11 10:31:03 +0900

    Edit 05장

--- src/App.js
+++ src/App.js
@@ -1,37 +1,28 @@
 import React from 'react';
-import img01 from './images/123.jpg';
-import img02 from './images/345.jpg';
-import img03 from './images/789.jpg';
-function Food({name,picture,message0}){
- 
-  return (
-    <div>
-          <h3>I Love {name}</h3>
-          <img src={picture} alt={message0}></img>
-    </div>
-           
-    )
+
+class App extends React.Component{
+  
+state = {
+  count: 0,
+
   
 }
-
-
-
-
-const foodILike = [
-  {id:1,name: '코알라', image: img01,message:'코알라사진'},
-  {id:2,name: '집', image: img02, message:'집사진'},
-  {id:3,name: '펭귄', image: img03, message:'펭귄사진'},
-];
-
-
-function App() {
-  // return <div className="App" />;
-      return (
-      <div>
-       {foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} message0={dish.message} />))}
-        
-      </div>
-      );
+  add = () =>{
+   // console.log('Add')
+    this.setState(current=> ({count: current.count + 1}));
+  };
+  minus = () =>{
+   // console.log('Minus')
+   this.setState(current=> ({count: current.count - 1}));
+  };
+  render(){
+  return(
+    <div>
+    <h1>The number is: {this.state.count}</h1>
+    <button onClick={this.add}>Add </button>&nbsp;&nbsp;&nbsp;&nbsp;
+    <button onClick={this.minus}>Minus</button>
+    </div>
+  );
 }
-
+}
 export default App;
