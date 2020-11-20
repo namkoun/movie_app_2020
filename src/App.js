e6b78207c19aff40d7ad70ffc1bb9625212289fa
namkoun <skarud0328@gmail.com>
2020-11-20 12:36:25 +0900

    7장,8장 업

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import { HashRouter, Route } from 'react-router-dom';
 import About from './routes/About';
 // import About from './routes/About';
 import Navigation from './components/Navigation';
-
+import Detail from './routes/Detail';
 function App() {
   return (
   <HashRouter>
     <Navigation />
      <Route path="/" exact={true} component={Home} />
      <Route path="/about" component={About} />
-   
+     <Route path="/movie-detail" component={Detail} />
   </HashRouter>
   );
 }
