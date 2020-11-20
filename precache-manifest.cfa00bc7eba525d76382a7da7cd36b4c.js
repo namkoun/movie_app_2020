be169775ef68a592ddf1a00c62956fbbc065f9be
namkoun <skarud0328@gmail.com>
2020-11-20 12:38:51 +0900

    Updates

---
+++ precache-manifest.cfa00bc7eba525d76382a7da7cd36b4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1affef608c9d3c1529d083c4c674478",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "24c444e1ff1cfc12d401",
+    "url": "/movie_app_2020/static/css/main.e1cd7773.chunk.css"
+  },
+  {
+    "revision": "e9d567958cdf7fa01b8b",
+    "url": "/movie_app_2020/static/js/2.3ce463e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.3ce463e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24c444e1ff1cfc12d401",
+    "url": "/movie_app_2020/static/js/main.83cc2297.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
